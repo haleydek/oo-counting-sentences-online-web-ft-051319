2a2a9680bd71cfb6639199335a58280a55f20941
Haley DeKeyser <hcd09@live.com>
2019-05-21 03:15:19 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,18 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self[-1] == "." ? true : false
   end
 
   def question?
-
+    self[-1] == "?" ? true : false
   end
 
   def exclamation?
-
+    self[-1] == "!" ? true : false
   end
 
   def count_sentences
-
+    self.scan(/[.?!]\s\w|\w[.?!]{2}|\w[.?!]\z/).count
   end
 end
